922bb7612e37cb3a3fcc9a6751081d620be30606
usman1100 <usmanahmed1100@gail.com>
2022-05-04 17:31:08 +0500

    More LL methods

--- linked-list/linked-ist.ts
+++ linked-list/linked-ist.ts
@@ -60,18 +60,69 @@ class LinkedList {
         }
     }
 
-    shift() {
+    shift(): ListNode {
         if (!this.head) throw Error("Stack underflow error");
 
         if (this.head.next === null) {
+            let copy = this.head;
+
             this.head = null;
             this.tail = null;
-            return;
+            return copy;
         }
 
+        let copy = this.head;
         let temp = this.head.next;
         this.head = null;
         this.head = temp;
+
+        return copy;
+    }
+
+    indexOf(value: number): number {
+        if (!this.head) return -1;
+        let index = 0;
+        let temp = this.head;
+
+        while (temp) {
+            if (temp.value === value) {
+                console.log(temp.value);
+                return index;
+            }
+            index++;
+            temp = temp.next as ListNode;
+        }
+
+        return -1;
+    }
+
+    remove(value: number) {
+        if (!this.head) throw Error("Stack underflow");
+
+        if (!this.head.next) {
+            this.pop();
+            return;
+        }
+
+        if (this.head.value === value) {
+            this.shift();
+            return;
+        }
+
+        let prev = this.head;
+        let temp = this.head.next;
+
+        while (temp) {
+            if (temp.value === value) {
+                temp = temp.next as ListNode;
+                prev.next = null;
+                prev.next = temp;
+                return;
+            }
+            temp = temp.next as ListNode;
+            prev = prev.next as ListNode;
+        }
+        throw Error("Not found");
     }
 
     view() {
@@ -92,12 +143,4 @@ ll.append(2);
 ll.append(3);
 ll.append(4);
 ll.append(5);
-ll.shift();
-ll.shift();
-ll.shift();
-ll.shift();
-ll.shift();
-
-console.log(ll.tail);
-
 console.log(ll.view());
